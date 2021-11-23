6422cc6dbdf2c8aface9bba90bd53fde457f415e
HeshamShawqy <Hesham.k.shawqy@gmail.com>
2021-11-23 20:23:43 +0200

    Phase_01
    Added Geocoder to search for location
    Added full screen button
    Navigation controls
    2D or 3D view

--- 2dmap.js
+++ 2dmap.js
@@ -122,3 +122,71 @@ map.on('load', () => {
     labelLayerId
     );
     });
+
+    // Add geocoder to the map(search buttom)
+const geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      types: 'poi',
+      // see https://docs.mapbox.com/api/search/#geocoding-response-object for information about the schema of each response feature
+      render: function (item) {
+      // extract the item's maki icon or use a default
+      const maki = item.properties.maki || 'marker';
+      return `<div class='geocoder-dropdown-item'>
+      <img class='geocoder-dropdown-icon' src='https://unpkg.com/@mapbox/maki@6.1.0/icons/${maki}-15.svg'>
+      <span class='geocoder-dropdown-text'>
+      ${item.text}
+      </span>
+      </div>`;
+      },
+      mapboxgl: mapboxgl
+      });
+
+class PitchToggle {
+    
+    constructor({bearing = -20, pitch = 50, minpitchzoom = null}) {
+        this._bearing = bearing;
+        this._pitch = pitch;
+        this._minpitchzoom = minpitchzoom;
+    }
+
+    onAdd(map) {
+        this._map = map;
+        let _this = this; 
+
+        this._btn = document.createElement('button');
+        this._btn.className = 'mapboxgl-ctrl-icon mapboxgl-ctrl-pitchtoggle-3d';
+        this._btn.type = 'button';
+        this._btn['aria-label'] = 'Toggle Pitch';
+        this._btn.onclick = function() { 
+            if (map.getPitch() === 0) {
+                let options = {pitch: _this._pitch, bearing: _this._bearing};
+                if (_this._minpitchzoom && map.getZoom() > _this._minpitchzoom) {
+                    options.zoom = _this._minpitchzoom;
+                }
+                map.easeTo(options);
+                _this._btn.className = 'mapboxgl-ctrl-icon mapboxgl-ctrl-pitchtoggle-2d';
+            } else {
+                map.easeTo({pitch: 0, bearing: 0});
+                _this._btn.className = 'mapboxgl-ctrl-icon mapboxgl-ctrl-pitchtoggle-3d';
+            } 
+        };
+        
+
+        this._container = document.createElement('div');
+        this._container.className = 'mapboxgl-ctrl mapboxgl-ctrl-group';
+        this._container.appendChild(this._btn);
+
+        return this._container;
+    }
+
+    onRemove() {
+        this._container.parentNode.removeChild(this._container);
+        this._map = undefined;
+    }
+
+}
+      map.addControl(geocoder);
+      map.addControl(new mapboxgl.FullscreenControl());
+      map.addControl(new mapboxgl.NavigationControl());
+      map.addControl(new PitchToggle({minpitchzoom: 15})); 
+    
